43def8cb956a20d1bf6adbcf84a1eca5e638c186
Oleg Kishinskiy <oleg.kishinskiy@gmail.com>
2025-09-29 20:55:11 +0300

    set default size

--- src/components/P/P.tsx
+++ src/components/P/P.tsx
@@ -3,7 +3,7 @@ import { PProps } from "./P.props";
 import styles from './P.module.css';
 import cn from 'classnames';
 
-export default function P({ size, children, className, ...props }: PProps): JSX.Element {
+export default function P({ size='medium', children, className, ...props }: PProps): JSX.Element {
     return (
         <p className={cn(styles.p, className, {
             [styles.small]: size === 'small',
